17e7d4cdc1df0203e332e30a651aa4644ee85207
Marc-Antoine Nadeau <nadeau.marc-antoine@hotmail.com>
2025-03-16 20:05:16 -0400

    Draft

--- Output/launch.sh
+++ Output/launch.sh
@@ -1,27 +1,18 @@
 #!/bin/bash
+#SBATCH --job-name=training
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:1
+#SBATCH --tasks-per-node=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=16G
+#SBATCH --time=04:00:00
+#SBATCH --output=%N-%j.out
+#SBATCH --account=winter2025-comp579 
 
-#SBATCH -p all                 # Partition (queue)
-#SBATCH -c 4                   # Number of cores
-#SBATCH --mem=4G               # Memory allocation
-#SBATCH --propagate=NONE       # Important for long jobs
-#SBATCH -t 0-2:00              # Time (D-HH:MM)
-#SBATCH -o slurm.%N.%j.out     # STDOUT file
-#SBATCH -e slurm.%N.%j.err     # STDERR file
-#SBATCH --qos=comp579-0gpu-4cpu-72h  # QoS for COMP 579 (Modify as needed)
-#SBATCH --account=winter2025-comp579 # Account to use for SOCS servers
+module load cuda/cuda-12.6 
+module load python/3.10
 
-module load miniconda/miniconda-fall2024  # Load necessary modules
+pip cache purge
+pip install -r requirements.txt
 
-# Add your Python commands or other necessary runs here
-# Example:
-# source activate my_env  # Activate your conda environment
-# python my_script.py     # Run your Python script
-
-# Notes for long-running jobs:
-# - Debug on your local machine first.
-# - Once everything is working, run on the server.
-# - Use the correct QoS based on job needs (e.g., CPU vs. GPU requirements).
-
-# Additional resources:
-# - SOCS GPU usage instructions: https://docs.sci.mcgill.ca/COMP/slurm/
-# - Contact slurm-admins.science@campus.mcgill.ca for assistance.
+python ../Code/code.ipynb
